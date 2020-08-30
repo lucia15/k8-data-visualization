a2af1b44e316da56212fd8b03d5aadf682797965
Sanchit <rajmbcoderx@gmail.com>
2020-08-30 23:35:45 +0530

    Formatted

--- upwork-devs/Giuseppe-mazzacua/issues.py
+++ upwork-devs/Giuseppe-mazzacua/issues.py
@@ -1,75 +1,71 @@
-import requests
-import json
-import pandas as pd 
-from pandas import json_normalize
-import matplotlib.pyplot as plt
-import seaborn as sns
+import      requests
+import      pandas as pd
+from        pandas import json_normalize
+import      matplotlib.pyplot as plt
+import      seaborn as sns
 
+""" Documentation for classes """
 class Issues:
-    def __init__(self, repos):
-        self.repos = repos
-        token = 'TOKEN'
-        self.headers = {'Authorization': f'token {token}'}
-        self.configure_pandas()
-        self.df = self.init_df()
 
+    def __init__(self, repos):
+        self.repos      = repos
+        token           = 'TOKEN'
+        self.headers    = {'Authorization': f'token {token}'}
+        self.configure_pandas()
+        self.df         = self.init_df()
+
+    """ Small documentation for methods """
     def init_df(self):
         try:
-            dfs = []
+            dfs         = []
             for repo in self.repos:
-                url = f'https://api.github.com/repos/filetrust/{repo}/issues'
-                res = requests.get(url, headers=self.headers, params={'state': 'all'}).json()
-                data = json_normalize(res, max_level=1)
-                temp_df = pd.DataFrame(data)
-                temp_df['repo']= repo
+                url                 = f'https://api.github.com/repos/filetrust/{repo}/issues'
+                res                 = requests.get(url, headers=self.headers, params={'state': 'all'}).json()
+                data                = json_normalize(res, max_level=1)
+                temp_df             = pd.DataFrame(data)
+                temp_df['repo']     = repo
                 dfs.append(temp_df)
-            df = pd.concat(dfs, ignore_index=True)
-            return df
+            df                      = pd.concat(dfs, ignore_index=True)
+            return                  df
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
     def get_df(self):
-        return self.df
+        return                      self.df
 
     def configure_pandas(self):
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-        pd.set_option('display.width', None)
-        pd.set_option('display.max_colwidth', -1)
-        pd.set_option('display.expand_frame_repr', False)
+        pd.set_option('display.max_rows'            , None)
+        pd.set_option('display.max_columns'         , None)
+        pd.set_option('display.width'               , None)
+        pd.set_option('display.max_colwidth'        , -1)
+        pd.set_option('display.expand_frame_repr'   , False)
 
     def normalize_data(self):
-        # Selecting Important Columns
-        df = self.df
-        df= df[['created_at','user.login', 'user.url','author_association', 'title','body', 'state', 'milestone.title','milestone.state','repo']]
-        # Creating date column
-        df['created_at'] = pd.to_datetime(df['created_at']).dt.date
-        self.df = df
+        df                      = self.df
+        df                      = df[['created_at','user.login', 'user.url','author_association', 'title','body', 'state', 'milestone.title','milestone.state','repo']]
+        df['created_at']        = pd.to_datetime(df['created_at']).dt.date
+        self.df                 = df
 
     def show_pie(self, column):
-        plt.figure(figsize=(8,8))
-        print(self.df[column].value_counts())
-        self.df[column].value_counts().plot(kind='pie',autopct='%.2f', fontsize=20)
+        plt.figure(figsize                                          =(8,8))
+        self.df[column].value_counts().plot(kind                    ='pie',autopct='%.2f', fontsize=20)
         plt.show()
 
     def show_bar_chart_by_repo(self):
-        #Visualize Repos with number of Issues
-        plt.figure(figsize=(15,10))
-        self.df["repo"].value_counts().plot.bar(title="Bar Chart Showing Number of Issues By Repo")
+        plt.figure(figsize                              = (15,10))
+        self.df["repo"].value_counts().plot.bar(title   = "Bar Chart Showing Number of Issues By Repo")
         plt.ylabel('Number of Issues')
         plt.xlabel('Repo')
         plt.show()
 
     def show_bar_chart_by_date(self):
-        #Visualize Dates with number of Issues
-        plt.figure(figsize=(40,10))
-        sns.countplot(self.df['created_at'],label="Number of Issues")
+        plt.figure(figsize                              = (40,10))
+        sns.countplot(self.df['created_at'],label       = "Number of Issues")
         plt.show()        
 
     def show_grid_chart(self, column):
-        #Visualize Repos with number of Issues
-        df = self.df
-        keys = [pair for pair, x in df.groupby([column])]
+        df                                              = self.df
+        keys                                            = [pair for pair, x in df.groupby([column])]
         plt.figure(figsize=(15,10))
         plt.plot(keys, df.groupby([column]).count())
         plt.xticks(keys)
@@ -77,12 +73,11 @@ class Issues:
         plt.show()
 
     def show_bar_chart_by_user(self):
-        # Number of Issues per Individual
-        df = self.df
-        user = df.groupby('user.login')
-        response = user.count()['created_at']
-        keys = [pair for pair, df in user]
-        plt.figure(figsize=(10,10))
+        df                                              = self.df
+        user                                            = df.groupby('user.login')
+        response                                        = user.count()['created_at']
+        keys                                            = [pair for pair, df in user]
+        plt.figure(figsize                              = (10,10))
         plt.bar(keys, response)
         plt.xticks(keys, rotation='vertical', size=8)
         plt.ylabel('Number of Issues')
