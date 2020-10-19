712d1c2e6ad7c97c4ebecfea3933d95f4c152a6b
Lucia Messina <lucia15.maga88@gmail.com>
2020-10-19 10:40:11 -0300

    Sort slides by project priority, delete projects with no priority value. Make the necessary changes for Daily Report new requirements.

--- upwork-devs/Pappaterra-Lucia/modules/power_point.py
+++ upwork-devs/Pappaterra-Lucia/modules/power_point.py
@@ -62,8 +62,8 @@ def logo(slide, img_path='images/glasswall_logo.png', place='top right'):
         
     elif place == 'top left':
         # Logo size
-        width = Inches(1.8)
-        height = Inches(0.9) # width half
+        width = Inches(1.25)
+        height = Inches(0.625) # width half
 
         # Place it on top left corner
         top = Inches(0.25) 
@@ -81,47 +81,88 @@ def fill(shape, fill_color=dark_blue):
     fill = shape.fill
     fill.solid()
     fill.fore_color.rgb = fill_color
-               
     
-def make_presentation(sheet_name, output_to, single=True):
-
-    prs = Presentation()
-
-    # PROJECT SLIDES
     
+def sheet_to_dfs(sheet_name):
+
     if sheet_name == 'Projects Team Structure':
     
         with open(os.path.join('yaml_files', sheet_name+'.yml'), 'r') as f:
             df = pd.json_normalize(yaml.safe_load(f))
             #df = pd.json_normalize(yaml.load(f, Loader=yaml.FullLoader))
+            
+        df['project_name'] = df['project_name'].apply(lambda s: s.rstrip('\n') if isinstance(s, str) else s)
+        
+        # consider only the projects that have some priority            
+        df0 = df[df['priority']!='']
+        df1 = df0[df0['priority']!=' ']
+        
+        # sort by priority
+        df1['priority'] = pd.Categorical(df1['priority'], categories=['One (1)','Two (2)','Three (3)','ByRequests'], ordered=True)            
+        df1 = df1.sort_values('priority')
+        
+        df1 = df1.reset_index(drop=True)
                     
         with open('yaml_files/Resource and responsability.yml', 'r') as f:
             df2 = pd.json_normalize(yaml.safe_load(f))
             #df2 = pd.json_normalize(yaml.load(f, Loader=yaml.FullLoader))
             
-        for row_index in range(len(df)):
-            add_project_slide(prs, df, row_index, df2)
+        return df1, df2
+               
+    
+def make_presentation(sheet_name, output_to, single=False, dm=False):
+
+    prs = Presentation()
+     
+    if sheet_name == 'Projects Team Structure':
+    
+        df1, df2 = sheet_to_dfs(sheet_name)
+        
+        # PROJECT SLIDES    
+        for row_index in range(len(df1)):
+            add_project_slide(prs, df1, row_index, df2)
             
             if single:           
                 # Also save single presentations to single folder
                 prs2 = Presentation()           
-                add_project_slide(prs2, df, row_index, df2)
-                p_name = df.iloc[row_index]['project_name'].rstrip('\n') 
+                add_project_slide(prs2, df1, row_index, df2)
+                p_name = df1.iloc[row_index]['project_name'].rstrip('\n') 
                 prs2.save('outputs/single/' + p_name + '.pptx')
+                
+        if dm:
+            # Also create a presentation per delivery manager to dm folder       
+            d_managers = list(df1['delivery_manager'].unique())
+            
+            for elem in d_managers:
+                if ',' in elem:
+                    d_managers.remove(elem)
+                       
+            for d_manager in d_managers:
+                prs3 = Presentation()
+                
+                df3 = df1[df1['delivery_manager'].str.contains(d_manager)]
+                df3 = df3.reset_index(drop=True)
+                
+                # PROJECT SLIDES    
+                for row_index in range(len(df3)):
+                    add_project_slide(prs3, df3, row_index, df2)
+                       
+                prs3.save('outputs/DM/' + d_manager + '.pptx')
 
     prs.save(output_to)
-    
+ 
 
 def add_project_slide(prs, df, row_index, df2):
 
-    p_name = df.iloc[row_index]['project_name'].rstrip('\n')
+    p_name = df.iloc[row_index]['project_name']
     p_priority = df.iloc[row_index]['priority']
     p_description = df.iloc[row_index]['short_project_description']
     
     p_stakeholder = df.iloc[row_index]['stakeholders'].split('\n')
-    p_stakeholder = list(filter(('').__ne__, p_stakeholder))
+    #filter empty string from list:
+    p_stakeholder = list(filter(('').__ne__, p_stakeholder)) 
     
-    p_manager = df.iloc[row_index]['delivery_manager'].replace(',', '\n').split('\n')
+    p_manager = df.iloc[row_index]['delivery_manager'].split(', ')
     p_manager = list(filter(('').__ne__, p_manager))
     
     p_security = df.iloc[row_index]['security_champ'].split('\n')
@@ -146,6 +187,8 @@ def add_project_slide(prs, df, row_index, df2):
         p = '2'
     elif '3' in p_priority:
         p = '3'
+    elif 'ByRequest' in p_priority:
+        p = 'by request'
     
     title.text = '\n' + '\n PROJECT: ' + p_name.upper()  + '\n' + p_description + '\n Priority: ' + p
     text_settings(title, i=0)
@@ -334,7 +377,12 @@ def add_slide(prs, df, row_index):
     next_release = df.iloc[row_index]['Next Release/ Important dates']
     comments = df.iloc[row_index]['If you have any more comments']
     
-    if len(next_release)>200 and comments == '':
+    #b_owner =
+    #p_users =
+    #parent_service =
+    #service_manager =
+    
+    if len(next_release)>250 and comments == '':
         comments = next_release[len(next_release)//2:]
         next_release = next_release[:len(next_release)//2]
     
@@ -356,92 +404,136 @@ def add_slide(prs, df, row_index):
     # TITLE
     title = shapes.title
     
-    title.text = 'DAILY PROJECT REPORT'
-    text_settings(title, i=0, alignment=PP_ALIGN.CENTER, font_size=Pt(24), font_color=dark_blue, bold=True)    
+    title.text = 'DAILY PROJECT REPORT ' + date +'\n' + p_name + '\n\n'
+    text_settings(title, i=0, alignment=PP_ALIGN.CENTER, font_size=Pt(10), font_color=dark_blue, bold=True)  
+    text_settings(title, i=1, alignment=PP_ALIGN.CENTER, font_size=Pt(24), font_color=dark_blue, bold=True)    
     
+    # FIGURES MEASURES:
+    
+    # distance to left side
     left = Inches(0.35) #1.25, 1.1, 0.8, 0.65, 0.35
-    top = Inches(1.4)
-    width0 = (Inches(10.0) - 2*left)/6
-    height = Inches(0.3)   
+    
+    # distance to top
+    top = Inches(1.1)
+    
+    # widths:
+    width0 = Inches(1.0) # Priority
+    width1 = Inches(0.9)
+    width2 = Inches(1.4) # RAG Status
+    width3 = Inches(1.1)
+    width4 = Inches(1.9) # Bussines Owner
+    width5 = Inches(3.0)
+    
+    width8 = Inches(10.0) - 2*left # Issues
+    width9 = width8/3 # In progress, Closed, Major Issues
+    
+    # Team, Onboarding, Offboarding, Next Release, Comments:
+    width10 = Inches(2.0) 
+    width11 = Inches(10.0) - 2*left - width10
+    width12 = (width11 - width10)/2
+    
+    # heights:
+    height0 = Inches(0.3) 
+    height1 = Inches(2.75)
+    
+    # separation between figures:
+    sep0 = Inches(0.1)
+    sep1 = Inches(0.03)
+      
     
     # First line
-    make_rectangle(shapes, 'Priority:', left, top, width0, height) 
-    make_rectangle(shapes, priority, left + width0, top, width0, height, font_color=dark_blue, fill_color=white, line_color=gray, alignment=PP_ALIGN.CENTER, bold=True)
+    make_rectangle(shapes, 'Priority:', left, top, width0, height0)
+    left_aux = left + width0
+    make_rectangle(shapes, priority, left_aux, top, width1, height0, font_color=dark_blue, fill_color=white, line_color=gray, alignment=PP_ALIGN.CENTER, bold=True)
     
-    make_rectangle(shapes, 'RAG Status:', left + 2*width0, top, width0, height)     
-    make_rectangle(shapes, rag_status, left + 3*width0, top, width0, height, font_color=dark_blue, fill_color=rgb_color, line_color=rgb_color, alignment=PP_ALIGN.CENTER, bold=True)
+    left_aux = left_aux + width1
+    make_rectangle(shapes, 'RAG Status:', left_aux, top, width2, height0)   
+    left_aux = left_aux + width2
+    make_rectangle(shapes, rag_status, left_aux, top, width3, height0, font_color=dark_blue, fill_color=rgb_color, line_color=rgb_color, alignment=PP_ALIGN.CENTER, bold=True)
     
-    make_rectangle(shapes, 'Date:', left + 4*width0, top, width0, height) 
-    make_rectangle(shapes, date, left + 5*width0, top, width0, height, font_color=dark_blue, fill_color=white, line_color=gray)
+    left_aux = left_aux + width3
+    make_rectangle(shapes, 'Bussines Owner:', left_aux, top, width4, height0)
+    left_aux = left_aux + width4 
+    make_rectangle(shapes, '', left_aux, top, width5, height0, font_color=dark_blue, fill_color=white, line_color=gray)
     
-    # Next line
-    sep = Inches(0.1)
-    top = top + height + sep
-    width1 = Inches(2.5)
-    left1 = left + width1    
-    width2 = Inches(10.0) - 2*left - width1
-    
-    make_rectangle(shapes, 'Project name:', left, top, width1, height)
-    make_rectangle(shapes, p_name, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray)
-    
-    # Next line 
-    top = top + height + sep
-    
-    make_rectangle(shapes, 'DM:', left, top, width1, height)  
-    make_rectangle(shapes, d_manager, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray)
     
     # Next line
-    top = top + height + sep
+    top = top + height0 + sep0 
     
-    make_rectangle(shapes, 'Team:', left, top, width1, height) 
-    make_rectangle(shapes, team, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray)
+    make_rectangle(shapes, 'Issues', left, top, width8, height0, alignment=PP_ALIGN.CENTER)
     
     # Next line
-    top = top + height + sep
+    top = top + height0 + sep1    
+    
+    make_rectangle(shapes, 'In Progress', left, top, width9, height0, alignment=PP_ALIGN.CENTER)
+    make_rectangle(shapes, 'Closed', left + width9, top, width9, height0, alignment=PP_ALIGN.CENTER)
+    make_rectangle(shapes, 'Major Issues', left + 2*width9, top, width9, height0, alignment=PP_ALIGN.CENTER)    
+    
+    # Next line
+    top = top + height0 + sep1
+    
+    make_rectangle(shapes, in_progress, left, top, width9, height1, font_color=red, fill_color=white, line_color=gray)
+    make_rectangle(shapes, closed, left + width9, top, width9, height1, font_color=red, fill_color=white, line_color=gray)
+    make_rectangle(shapes, major_issues, left + 2*width9, top, width9, height1, font_color=red, fill_color=white, line_color=gray)
+    
+    # Next line
+    top = top + height1 + sep0
+    
+    left1 = left + width10  
+    left2 = left1 + width12
+    left3 = left1 + width12 + width10
       
-    make_rectangle(shapes, 'Onboarding:', left, top, width1, height)
-    make_rectangle(shapes, onboarding, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray) 
-    
-    # Next line
-    sep2 = Inches(0.03)
-    top = top + height + sep2
+    make_rectangle(shapes, 'Delivery Manager:', left, top, width10, height0)
+    make_rectangle(shapes, d_manager, left1, top, width12, height0, font_color=dark_blue, fill_color=white, line_color=gray) 
          
-    make_rectangle(shapes, 'Offboarding:', left, top, width1, height) 
-    make_rectangle(shapes, offboarding, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray)
+    make_rectangle(shapes, 'Project Users:', left2, top, width10, height0) 
+    make_rectangle(shapes, '', left3, top, width12, height0, font_color=dark_blue, fill_color=white, line_color=gray)
+      
+    # Next line 
+    top = top + height0 + sep0  
+       
+    make_rectangle(shapes, 'Team:', left, top, width10, height0) 
+    make_rectangle(shapes, team, left1, top, width11, height0, font_color=dark_blue, fill_color=white, line_color=gray)
     
     # Next line
-    top = top + height + sep
-    width3 = Inches(10.0) - 2*left 
+    top = top + height0 + sep0
     
-    make_rectangle(shapes, 'Issues', left, top, width3, height, alignment=PP_ALIGN.CENTER)
+    if onboarding == 'None':
+        color = dark_blue
+    else:
+        color = red
+      
+    make_rectangle(shapes, 'Onboarding:', left, top, width10, height0)  
+    make_rectangle(shapes, onboarding, left1, top, width12, height0, font_color=color, fill_color=white, line_color=gray) 
     
-    # Next line
-    top = top + height + sep2
-    width4 = width3/3
-    
-    make_rectangle(shapes, 'In progress', left, top, width4, height, alignment=PP_ALIGN.CENTER)
-    make_rectangle(shapes, 'Closed', left + width4, top, width4, height, alignment=PP_ALIGN.CENTER)
-    make_rectangle(shapes, 'Major Issues', left + 2*width4, top, width4, height, alignment=PP_ALIGN.CENTER)
-    
-    # Next line
-    top = top + height + sep2
-    height1 = Inches(2.0)
-    
-    make_rectangle(shapes, in_progress, left, top, width4, height1, font_color=dark_blue, fill_color=white, line_color=gray)
-    make_rectangle(shapes, closed, left + width4, top, width4, height1, font_color=dark_blue, fill_color=white, line_color=gray)
-    make_rectangle(shapes, major_issues, left + 2*width4, top, width4, height1, font_color=dark_blue, fill_color=white, line_color=gray)
+    if offboarding == 'None':
+        color = dark_blue
+    else:
+        color = red
+         
+    make_rectangle(shapes, 'Offboarding:', left2, top, width10, height0) 
+    make_rectangle(shapes, offboarding, left3, top, width12, height0, font_color=color, fill_color=white, line_color=gray)
     
     # Next line
-    top = top + height1 + sep
+    top = top + height0 + sep0
     
-    make_rectangle(shapes, 'Next Release:', left, top, width1, height) 
-    make_rectangle(shapes, next_release, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray)
+    make_rectangle(shapes, 'Parent Service:', left, top, width10, height0)
+    make_rectangle(shapes, '', left1, top, width12, height0, font_color=dark_blue, fill_color=white, line_color=gray) 
+         
+    make_rectangle(shapes, 'Service Manager:', left2, top, width10, height0) 
+    make_rectangle(shapes, '', left3, top, width12, height0, font_color=dark_blue, fill_color=white, line_color=gray)
+    
+    # Next line
+    top = top + height0 + sep0
+    
+    make_rectangle(shapes, 'Next Release:', left, top, width10, height0) 
+    make_rectangle(shapes, next_release, left1, top, width11, height0, font_color=red, fill_color=white, line_color=gray)
         
     # Next line
-    top = top + height + sep
+    top = top + height0 + sep0
     
-    make_rectangle(shapes, 'Comments:', left, top, width1, height) 
-    make_rectangle(shapes, comments, left1, top, width2, height, font_color=dark_blue, fill_color=white, line_color=gray)
+    make_rectangle(shapes, 'Comments:', left, top, width10, height0) 
+    make_rectangle(shapes, comments, left1, top, width11, height0, font_color=red, fill_color=white, line_color=gray)
 
     
 def make_rectangle(shapes, sentence, left, top, width, height, font_color=white, fill_color=dark_blue, line_color=dark_blue, alignment=PP_ALIGN.LEFT, bold=False, font_size=Pt(14)):
@@ -451,14 +543,16 @@ def make_rectangle(shapes, sentence, left, top, width, height, font_color=white,
     
     if height >= Inches(2.0):
         if len(shape.text)>550:
-            font_size=Pt(8)  
-        elif len(shape.text)>=340:
+            font_size=Pt(9)          
+        elif len(shape.text)>=500:  
+            font_size=Pt(10)
+        elif len(shape.text)>=350:
             font_size=Pt(11)
         elif len(shape.text)>200: 
-            font_size=Pt(13)
+            font_size=Pt(12)
     else: 
-        if len(shape.text)>80:
-            font_size=Pt(10)   
+        if len(shape.text)>100:
+            font_size=Pt(11)   
     
     text_settings(shape, font_color=font_color, alignment=alignment, bold=bold, font_size=font_size)
     
